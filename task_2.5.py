41a2182e9e2e6654169b072c351ebccba4b14f57
yuliyaK <danechekspb@yandex.ru>
2021-10-02 21:52:14 +0300

    add homework 1

---
+++ task_2.5.py
@@ -0,0 +1,17 @@
+my_list = [7, 5, 3, 3, 2]
+number = int(input("Введите натуральное число: "))
+num = my_list.count(number)
+
+for element in my_list:
+    if num > 0:
+        a = my_list.index(number)
+        my_list.insert(a+num, number)
+        break
+    else:
+        if number > element:
+            b = my_list.index(element)
+            my_list.insert(b, number)
+            break
+        elif number < my_list[len(my_list) - 1]:
+            my_list.append(number)
+print(my_list)
